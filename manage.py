b7e0c4b0a9663956c1b98c18db26a38968053ba8
leng <1622571327@qq.com>
2018-04-11 13:42:26 +0800

    最简单的应用

---
+++ manage.py
@@ -0,0 +1,17 @@
+#!/user/bin/env python
+# -*- coding:utf-8 -*-
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'hello'
+
+@app.route('/<name>',methods=['GET'])
+def welcome(name):
+    return 'welcome %s' % name
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
